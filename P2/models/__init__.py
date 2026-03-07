c74e87f9e28061a4d1accacd959d9c5d422a0fe5
New-Rookie <125655273+New-Rookie@users.noreply.github.com>
2026-03-07 14:02:37 +0800

    Delete P2 directory

--- P2/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .gcn import GCNEncoder
-from .mappo_gcn import MAPPOGCNAgent
-from .mappo import MAPPOAgent
-from .maddpg import MADDPGAgent
-from .greedy import GreedyLinkSelector
-from .ant_colony import ACOLinkSelector
